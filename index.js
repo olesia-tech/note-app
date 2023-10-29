e734cab8caba5034a7d57feb6b93f3bffdea9882
olesia-tech <shikotanshop@gmail.com>
2023-10-29 18:42:52 -0400

    Initial commit: Base structure for note-app, to be continued

---
+++ index.js
@@ -0,0 +1,43 @@
+const btnEl = document.getElementById('btn');
+const appEl = document.getElementById('app');
+
+
+function createNoteEl(id, content){
+const element = document.createElement('textarea');
+element.classList.add('note');
+element.placeholder = 'Empty Note';
+element.value = content;
+
+element.addEventListener('dblclick', ()=> {
+  const warning = confirm('Do you want to delete this note?')
+  if(warning){
+    deleteNote(id, element)
+  }
+})
+
+element.addEventListener('input', ()=>{
+  updateNote(id, element.value)
+})
+}
+
+
+function deleteNote(){
+
+}
+
+function updateNote(){
+
+}
+
+
+function addNote(){
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: ""
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl) 
+}
+
+
+btnEl.addEventListener('click', addNote)
