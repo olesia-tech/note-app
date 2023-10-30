b12dcc93d882e02eccda74e051c4a21b42cbb026
olesia-tech <shikotanshop@gmail.com>
2023-10-30 09:49:18 -0400

    completed note-app

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const btnEl = document.getElementById('btn');
 const appEl = document.getElementById('app');
 
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl)
+})
 
 function createNoteEl(id, content){
 const element = document.createElement('textarea');
@@ -13,30 +17,49 @@ element.addEventListener('dblclick', ()=> {
   if(warning){
     deleteNote(id, element)
   }
-})
-
+});
 element.addEventListener('input', ()=>{
   updateNote(id, element.value)
-})
+});
+return element;
 }
 
 
-function deleteNote(){
 
+function deleteNote(id, element){
+const notes = getNotes().filter((note) => note.id !== id);
+saveNoteLocalStorage(notes);
+appEl.removeChild(element);
 }
 
-function updateNote(){
-
+function updateNote(id, content){
+const notes = getNotes();
+const target = notes.filter((note) => note.id == id)[0];
+target.content = content;
+saveNoteLocalStorage(notes)
 }
 
 
 function addNote(){
-  const noteObj = {
+ const notes = getNotes();
+ const noteObj = {
     id: Math.floor(Math.random() * 100000),
     content: ""
   };
   const noteEl = createNoteEl(noteObj.id, noteObj.content);
-  appEl.insertBefore(noteEl, btnEl) 
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+  
+  saveNoteLocalStorage(notes);
+}
+
+function saveNoteLocalStorage(notes){
+localStorage.setItem('note-app', JSON.stringify(notes))
+}
+
+function getNotes(){
+return JSON.parse(localStorage.getItem('note-app') || '[]');
 }
 
 
